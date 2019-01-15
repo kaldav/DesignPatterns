be405be9c17010e733b269305cfca560420b56c7
Dávid Kalácska <david.kalacska@dealogic.com>
2019-01-15 09:29:26 +0100

    added builder pattern

---
+++ CreationalBuilderPattern/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalBuilderPattern
+{
+    class Shop
+
+    {
+        // Builder uses a complex series of steps
+
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+}
