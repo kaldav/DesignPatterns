05123ace93fa009cff0f6e13b9930979185b5f60
Dávid Kalácska <david.kalacska@dealogic.com>
2019-01-15 09:34:23 +0100

    added strategy pattern

---
+++ BehavioralStrategyPattern/SortStrategy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehavioralStrategyPattern
+{
+    abstract class SortStrategy
+    {
+        public abstract void Sort(List<string> list);
+    }
+
+    class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort(); // Default is Quicksort
+
+            Console.WriteLine("QuickSorted list ");
+        }
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteStrategy' class
+
+    /// </summary>
+
+    class ShellSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.ShellSort(); not-implemented
+
+            Console.WriteLine("ShellSorted list ");
+        }
+    }
+
+    /// <summary>
+
+    /// A 'ConcreteStrategy' class
+
+    /// </summary>
+
+    class MergeSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            //list.MergeSort(); not-implemented
+
+            Console.WriteLine("MergeSorted list ");
+        }
+    }
+}
